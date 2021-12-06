8cd43bab4ba4eaeef1913fea96571a0b79045f58
nabet <yoan.lopez@utp.edu.co>
2021-12-06 18:43:04 -0500

    deploy

--- core/settings.py
+++ core/settings.py
@@ -106,13 +106,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(default='sqlite:///'+BASE_DIR / 'db.sqlite3')
+''' DATABASES = {
     'default':
         {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
-}
+} '''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
